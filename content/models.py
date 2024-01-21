d48d72d0832b2f85e523db7149b41e1558121730
moshtaghi <a.moshtaghi@runcorp.net>
2024-01-21 11:25:32 +0330

    add another user

--- content/models.py
+++ content/models.py
@@ -6,6 +6,9 @@ class Content(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
 
+    def __str__(self):
+        return self.title
+
 
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
